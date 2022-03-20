dee06ee53876029f74076daf5e9ee8c5e4b6ceaa
Daniel Morrison <1013921026@qq.com>
2022-03-20 16:21:32 +0800

    fix: 首页样式

---
+++ src/components/AdvanceInfoCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div
+        class="advances__card cursor-default group h-40 my-auto bg-white border-slate-200 hover:border-indigo-600 border-2 hover:bg-indigo-600 rounded-xl p-5 shadow-xl hover:shadow-2xl text-black hover:text-white transition-all grid grid-cols-3 gap-4"
+    >
+        <img class="row-span-2 m-auto" :src="props.infoObj.imgUrl" width="60" alt />
+        <div class="flex flex-row justify-start items-end">
+            <span
+                class="text-black group-hover:text-white col-span-2 text-xl font-bold"
+            >{{ props.infoObj.title }}</span>
+        </div>
+        <span class="text-black group-hover:text-white col-span-2 text-sm font-thin">
+            <p>{{ props.infoObj.desc }}</p>
+            <p>{{ props.infoObj.desc2 }}</p>
+        </span>
+    </div>
+</template>
+
+<script setup>
+import imgFlash from '@/assets/images/flash.png';
+const props = defineProps({
+    infoObj: {
+        type: Object,
+        default: () => {
+            return {
+                imgUrl: imgFlash,
+                title: '出租快',
+                desc: '最快可当天签约',
+                desc2: '提供装修并配置品牌家具家电',
+            }
+        }
+    },
+});
+
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,26 @@
         </div>
       </div>
     </div>
+    <div class="container mx-auto">
+      <section class="landlord__wrap py-5">
+        <div class="top-title-text text-4xl my-10 pl-4 md:pl-2 relative">
+          <h1>房屋出租 成为业主</h1>
+        </div>
+        <div class="advances__wrap grid grid-cols-3 gap-4 px-4">
+          <AdvanceInfoCardVue v-for="(item, index) in advances" :key="index" :infoObj="item" />
+        </div>
+        <div class="landlore__btn px-5 py-10 flex flex-col justify-center items-center">
+          <el-link
+            :underline="false"
+            @click="onNewHouseClick"
+            class="query-btn inline-flex mt-5 w-80 h-24 px-5 py-3 items-center justify-center rounded-md border border-transparent bg-indigo-600 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-indigo-500 focus:outline-none"
+          >
+            <span class="text-2xl">发布房源</span>
+          </el-link>
+        </div>
+      </section>
+    </div>
+
     <!-- <div
       class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:flex-col lg:items-center lg:justify-between lg:py-16 lg:px-8"
     >
@@ -36,6 +56,40 @@
 
 <script setup>
 import HouseSearchInputVue from '@/components/HouseSearchInput.vue';
+import AdvanceInfoCardVue from '@/components/AdvanceInfoCard.vue';
+import imgFlash from '@/assets/images/flash.png';
+import imgDiamond from '@/assets/images/diamond.png';
+import imgMoney from '@/assets/images/money.png';
+
+const advances = [
+  {
+    imgUrl: imgFlash,
+    title: '出租快',
+    desc: '最快可当天签约',
+    desc2: '提供装修并配置品牌家具家电',
+  },
+  {
+    imgUrl: imgDiamond,
+    title: '租客好',
+    desc: '租客对接公安系统',
+    desc2: '多维度保障安全',
+  },
+  {
+    imgUrl: imgMoney,
+    title: '收益稳',
+    desc: '按期打款，赠送财险',
+    desc2: '保障不动产安全',
+  },
+]
+
+/**
+ * @function onNewHouseClick
+ * @description 点击发布房源按钮
+ */
+const onNewHouseClick = () => {
+  console.log('onNewHouseClick');
+}
+
 </script>
 
 <style>
